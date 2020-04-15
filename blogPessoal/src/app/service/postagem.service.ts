4f6b74fb0f88f76a3d9ae3ba0a7d3f6fab01756f
thiagoagduarte <thiagoagduarte@gmail.com>
2020-04-15 13:47:51 -0300

    Criação do endPoint GetAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // CRUD - CREATE READ UPDATE E DELETE
+
+  // END POINT - DECLARAR NO SERVICE TODOS OS PONTOS DE CONTATO COM O SERVIDOR UTILIZANDO CRUD
+
+  // INICIO DAS OPERAÇÕES
+
+  // READ
+  getAllPostagens() {
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
 }
