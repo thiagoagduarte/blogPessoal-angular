498ddf89036c291c63ae1fba9d9755100f0265cb
thiagoagduarte <thiagoagduarte@gmail.com>
2020-04-27 14:42:16 -0300

    Implementação do postPostagem

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class PostagemService {
 
   constructor(private http: HttpClient) { }
 
-  // CRUD - CREATE READ UPDATE E DELETE
+  // CRUD - CREATE(POST) READ(GET) UPDATE(PUT) E DELETE(DELETE)
 
   // END POINT - DECLARAR NO SERVICE TODOS OS PONTOS DE CONTATO COM O SERVIDOR UTILIZANDO CRUD
 
@@ -17,6 +18,10 @@ export class PostagemService {
   // READ
   getAllPostagens() {
     return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
+  postPostagem(postagem: Postagem) {
+    return this.http.post('http://31.220.57.14:8080/postagens', postagem)
   }
 
 }
